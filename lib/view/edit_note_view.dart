06ee92b716f23afe6d5887bcb521cb887abf8a6e
Mohamed-Dawood9 <mboa990@gmail.com>
2024-04-13 18:05:22 +0200

    correct

--- lib/view/edit_note_view.dart
+++ lib/view/edit_note_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/cubit/note_cubit/note_cubit.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
@@ -9,29 +8,26 @@ class EditNoteView extends StatelessWidget {
   static String id = "editNoteView";
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => NoteCubit(),
-      child: const Scaffold(
-        body: Padding(
-          padding: EdgeInsets.only(left: 20, right: 20, top: 8),
-          child: Column(
-            children: [
-              SizedBox(height: 40),
-              CustomAppBar(
-                title: "Edit Note",
-                icon: Icons.check,
-              ),
-              SizedBox(height: 40),
-              CustomTextFormField(hint: "Title"),
-              SizedBox(
-                height: 16,
-              ),
-              CustomTextFormField(
-                hint: "\n\nContent\n\n",
-                maxLines: 5,
-              ),
-            ],
-          ),
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(left: 20, right: 20, top: 8),
+        child: Column(
+          children: [
+            SizedBox(height: 40),
+            CustomAppBar(
+              title: "Edit Note",
+              icon: Icons.check,
+            ),
+            SizedBox(height: 40),
+            CustomTextFormField(hint: "Title"),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextFormField(
+              hint: "\n\nContent\n\n",
+              maxLines: 5,
+            ),
+          ],
         ),
       ),
     );
